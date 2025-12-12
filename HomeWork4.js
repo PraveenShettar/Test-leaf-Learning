b7eb17e0fe42f37e35d509438f0fef5ff5678833
PraveenShettar <132926250+PraveenShettar@users.noreply.github.com>
2025-12-13 00:15:29 +0530

    Add files via upload

---
+++ HomeWork4.js
@@ -0,0 +1,20 @@
+ let browser = "Chrome"
+
+ if (browser==="Chrome"){
+    console.log("Launched Browser:"+browser);
+    
+ }else{
+   console.log("Launched Browser:"+"Other Browser");
+    
+ }
+
+ let testType= "smoke"
+
+ if (testType==="smoke"){
+    console.log("Test type is:"+testType);
+    
+ }else if(testType=="santity"){
+     console.log("Test type is:"+"sanity");
+ }else {
+     console.log("Test type is:"+"sanity");
+ }
