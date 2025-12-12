d687cb7cc812bf96c3fea65a3efa49e6d400711b
PraveenShettar <132926250+PraveenShettar@users.noreply.github.com>
2025-12-13 00:08:31 +0530

    Add files via upload

---
+++ HomeWork3.js
@@ -0,0 +1,13 @@
+ let num =0
+
+ if(num>0){
+    console.log("Positive Number");
+    
+ }else if(num<0){
+    console.log("Negative Number");
+
+    
+ }else{
+    console.log("Neutral Number");
+    
+ }
