c5a012d2095a602f278e363dd1183574e1642d91
PraveenShettar <132926250+PraveenShettar@users.noreply.github.com>
2025-12-12 23:58:33 +0530

    Add files via upload

---
+++ HomeWork.js
@@ -0,0 +1,12 @@
+
+let firstName = "Praveen"
+let companyName = "Access"
+let mobileNumber = "9876543210"
+let isAutomation = false
+let hasPlaywright
+
+console.log(firstName);
+console.log(companyName);
+console.log(mobileNumber);
+console.log(isAutomation);
+console.log(hasPlaywright);
