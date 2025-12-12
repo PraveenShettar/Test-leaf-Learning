c810b0a35e7b0d8e49a4ed6691c6dd54b0c5beeb
PraveenShettar <132926250+PraveenShettar@users.noreply.github.com>
2025-12-13 00:26:08 +0530

    Add files via upload

---
+++ Loop.js
@@ -0,0 +1,27 @@
+let Mark =35
+
+if (Mark>=85){
+    console.log("Grade A");
+}    
+else if (Mark>=60){
+    console.log("Grade B");
+}
+else if (Mark>=50){
+    console.log("Grade C");
+}   
+else{
+    console.log("Grade D");
+}
+
+// class 2
+
+// let num = 30
+// for (let i= 0; i<=num; i++) {
+//     if (i%2!==0){
+//          console.log("Odd Number"+ i);
+         
+//     }
+    
+    
+    
+// }
